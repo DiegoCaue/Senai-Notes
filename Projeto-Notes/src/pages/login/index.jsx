52c7f86c441062101d3e1a0124a33115402efe57
Diego <diegocaue7@gmail.com>
2025-05-20 21:00:39 -0300

    Implementando Usestate

--- Projeto-Notes/src/pages/login/index.jsx
+++ Projeto-Notes/src/pages/login/index.jsx
@@ -1,64 +1,105 @@
 import "./login.css"
 import logo from "../../assets/imgs/senaiNotesLogo.svg"
+import { useState } from "react"
 
 
+function Login() {
 
-function Login () {
+    const [email, SetEmail] = useState("")
+    const [password, SetPassword] = useState ("")
+    
+
+    const  ClickParaLogar = async () => {
+        if (email == "Diego@teste.com" && password == "123456") {
+
+            let token = "meuToken"
+            let userId = "meuID"
+
+            localStorage.setItem("meuToken", token)
+            localStorage.setItem("meuId", userId )
+
+        alert ("Login Realizado com sucesso")    
+
+        window.location.href = "/TelaNotes"
+    }
+     
+    else {
+        alert ("Credenciais incorretas"); 
+        return;
+    }
+
+    }  
+
+    const ClickParaCadastrar = async () => {
+        window.location.href = "/new-user"
+
+    }
+
     return (
         <>
-<div class="caixa-do-login">
-        <div class="logodoSenai"><img src={logo} alt=""/></div>
+            <div className="caixa-do-login">
+                <div className="logodoSenai"><img src={logo} alt="" /></div>
 
-        <div class="Welcome">
-            <h2>Welcome to Note</h2>
-            <p>Please log in to continue</p>
-        </div>
-
-        <div class="caixa-de-logar">
-
-            <div class="email">
-                <p><strong>Email Address</strong></p>
-                <div class="input-email"> <input type="email" placeholder="email@example.com"/></div>
-            </div>
-
-            <div class="password">
-
-                <div class="text-password">
-                    <p><strong>Password</strong></p>
-                    <div class="text-Forgot"><button class="btn-password">
-                            <p>Forgot</p></button>
-                    </div>
+                <div className="Welcome">
+                    <h2>Welcome to Note</h2>
+                    <p>Please log in to continue</p>
                 </div>
 
-                <div class="caixa-input">
+                <div className="caixa-de-logar">
 
-                    <div class="input-password">
-                        <input type="password"/>
-                    
+                    <div className="email">
+                        <p><strong>Email Address</strong></p>
+                        <div className="input-email"> 
+                            <input type="email" 
+                            placeholder="email@example.com" 
+                            value={email} 
+                            onChange={event => SetEmail(event.target.value)}
+                                           
+                        /></div>
                     </div>
+
+                    <div className="password">
+
+                        <div className="text-password">
+                            <p><strong>Password</strong></p>
+                            <div className="text-Forgot"><button className="btn-password">
+                                <p>Forgot</p></button>
+                            </div>
+                        </div>
+
+                        <div className="caixa-input">
+
+                            <div className="input-password">
+                                <input type="password" 
+                                value={password} 
+                                onChange={event => SetPassword(event.target.value)}
+                                
+                                />
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className="barra"></div>
+
+                    <div className="btn"><button className="btn" onClick={() =>ClickParaLogar()}>
+                        <p>Login</p>
+                    </button></div>
 
                 </div>
 
+                <div className="Cadastrar">
+                    <div className="text-rigth">
+                        <p>No account yet?</p>
+                    </div>
+                    <button className="btn-SignUp" onClick={() =>ClickParaCadastrar()}>
+                        <p>Sign Up</p>
+                    </button>
+                </div>
+
             </div>
-
-            <div class="barra"></div>
-
-            <div class="btn"><button class="btn">
-                    <p>Login</p>
-                </button></div>
-
-        </div>
-
-        <div class="Cadastrar">
-            <div class="text-rigth">
-                <p>No account yet?</p>
-            </div>
-            <button class="btn-SignUp">
-                <p>Sign Up</p>
-            </button>
-        </div>
-
-    </div>
         </>
     )
 }
--- Projeto-Notes/src/pages/new-user/index.jsx
+++ Projeto-Notes/src/pages/new-user/index.jsx
@@ -1,70 +1,76 @@
 import "./new-user.css"
 
 
-function NewUser () {
+function NewUser() {
+
+    const ClickParaLogar = async () =>  {
+
+        window.location.href = "/login"
+ 
+    }
 
     return (
         <>
-         <div class="caixa-do-login">
-        <div class="logodoSenai"><img src="../Arquivos/Logo Wrapper.svg" alt=""/></div>
+            <div class="caixa-do-login">
+                <div class="logodoSenai"><img src="../Arquivos/Logo Wrapper.svg" alt="" /></div>
 
-        <div class="welcome">
-        <div class="subtitulo">
-            <h2>Create Your Account</h2> 
-        </div>
-        <p>Sign up to start organizing your notes and boost your productivity.</p>
-        
-    </div>
-
-        <div class="caixa-de-logar">
-
-            <div class="email">
-                <p><strong>Email Address</strong></p>
-                <div class="input-email"> <input type="email" placeholder="email@example.com"/></div>
-            </div>
-
-            <div class="password">
-
-                <div class="text-password">
-                    <p><strong>Password</strong></p>
-                    <div class="text-Forgot"><button class="btn-password">
-                            <p>Forgot</p></button>
+                <div class="welcome">
+                    <div class="subtitulo">
+                        <h2>Create Your Account</h2>
                     </div>
-                </div>
-
-                <div class="caixa-input">
-
-                    <div class="input-password">
-                        <input type="password"/>
-                        <img src="../SenaiNotes/assets/img/ocutarSenha.svg" alt=""/>
-                    </div>
-                  <div class="caracteres">
-                    <img src="../SenaiNotes/assets/img/info circle.svg" alt="" />
-                  
-                  </div>
+                    <p>Sign up to start organizing your notes and boost your productivity.</p>
 
                 </div>
 
+                <div class="caixa-de-logar">
+
+                    <div class="email">
+                        <p><strong>Email Address</strong></p>
+                        <div class="input-email"> <input type="email" placeholder="email@example.com" /></div>
+                    </div>
+
+                    <div class="password">
+
+                        <div class="text-password">
+                            <p><strong>Password</strong></p>
+                            <div class="text-Forgot"><button class="btn-password">
+                                <p>Forgot</p></button>
+                            </div>
+                        </div>
+
+                        <div class="caixa-input">
+
+                            <div class="input-password">
+                                <input type="password" />
+                                <img src="../SenaiNotes/assets/img/ocutarSenha.svg" alt="" />
+                            </div>
+                            <div class="caracteres">
+                                <img src="../SenaiNotes/assets/img/info circle.svg" alt="" />
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="barra"></div>
+
+                    <div class="btn"><button class="btn">
+                        <p>Login</p>
+                    </button></div>
+
+                </div>
+
+                <div class="Cadastrar">
+                    <div class="text-rigth">
+                        <p>Already have an account?</p>
+                    </div>
+                    <button class="btn-SignUp" onClick={() => ClickParaLogar()}>
+                        <p>Login</p>
+                    </button>
+                </div>
+
             </div>
-
-            <div class="barra"></div>
-
-            <div class="btn"><button class="btn">
-                    <p>Login</p>
-                </button></div>
-
-        </div>
-
-        <div class="Cadastrar">
-            <div class="text-rigth">
-                <p>Already have an account?</p>
-            </div>
-            <button class="btn-SignUp">
-                <p>Login</p>
-            </button>
-        </div>
-
-    </div>
         </>
     )
 
